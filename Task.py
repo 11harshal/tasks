e3381c0c195ad20d8ce2b946604aa64540d83456
11harshal <113329800+11harshal@users.noreply.github.com>
2022-09-12 11:06:23 +0530

    tasks
    class

---
+++ Task.py
@@ -0,0 +1,53 @@
+# Task 1
+# n=0
+# while True:
+#     if n<3: 
+#         print(n)
+#         n+=1
+# Task 2
+# a=input("Enter the value")
+
+# if a=="pythoneishelpfull":
+#     print("Correct password")
+# else: 
+#     print("incorrect password")
+#     print("your password"+a)
+# Task 3
+# pwd="pythone"
+# while True: 
+#     a =input("Enter the value")
+#     if a== pwd:
+#        print("correct pass")
+#        break
+#     elif a!=pwd:
+#         print("incorrect pass")
+#         pwd= a 
+# task 4
+# create array
+#answers in array 
+# a=[15,12.7,24.01,19]
+# i=0
+# print(",end=")
+
+# while True:
+#     if len(a)>i:
+#         print(a[i]**2,",",)
+#         i=i+1
+#     else:
+#         break
+#     print("]")  
+# # task 5
+# count=0
+# tot=0
+# while True:
+#     min=input("Enter the number")
+#     # if min=="finsh"or min=="done"or min=="complete"or min=="4":
+#     if min==("finish","done","complete")or min=="4":
+#         break
+#     try:
+#         min =float(min)
+#         count=count+1
+#         tot=tot+min
+#     except:
+#         print("invalid")
+# print(count,tot)
